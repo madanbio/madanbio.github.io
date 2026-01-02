434d8120dadebe5069b3da0b2352f38af2562f6a
madanbio <madanuideveloper@gmail.com>
2026-01-02 17:41:34 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./assets/css/base7dd3.css";
-import "./assets/css/magnific7dd3.css";
-import "./assets/css/animated-headlines7dd3.css";
-import "./assets/css/style7dd3.css";
-import "./assets/js/jquery7dd3.js";
-import "./assets/js/isotope.js";
-import "./assets/js/magnific.js";
-import "./assets/js/animated-headlines7dd3.js";
-import "./assets/js/waypoints7dd3.js";
-import "./assets/js/app.js";
-import "./assets/js/init7dd3.js";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
