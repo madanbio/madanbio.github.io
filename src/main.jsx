d0b5ff46127095d93929c88b8aa02dc4149091fb
madanbio <madanuideveloper@gmail.com>
2026-01-06 12:12:55 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './assets/css/base7dd3.css'
+import './assets/css/magnific7dd3.css'
+import './assets/css/animated-headlines7dd3.css'
+import './assets/css/style7dd3.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
