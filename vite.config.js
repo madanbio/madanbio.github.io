c0fa09218418d16803e11f234e98225564f07f90
madanbio <madanuideveloper@gmail.com>
2026-01-07 12:12:30 +0530

    Add docs folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/madanbio.github.io/",  // GitHub Pages repo path
+  base: "/",  // GitHub Pages repo path
   build: {
     outDir: "docs",               // Build directly into docs folder
   },
