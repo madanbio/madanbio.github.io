59f7b5dbf381623ec8742514e718251086162269
madanbio <madanuideveloper@gmail.com>
2026-01-07 12:27:57 +0530

    Build React app for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./assets/css/base7dd3.css";
-import "./assets/css/magnific7dd3.css";
-import "./assets/css/animated-headlines7dd3.css";
-import "./assets/css/style7dd3.css";
-import "./assets/js/jquery7dd3.js";
-import "./assets/js/isotope.js";
-import "./assets/js/magnific.js";
-import "./assets/js/animated-headlines7dd3.js";
-import "./assets/js/waypoints7dd3.js";
-import "./assets/js/app.js";
-import "./assets/js/init7dd3.js";
 
+// CSS
+import "/public/assets/css/base7dd3.css";
+import "/public/assets/css/magnific7dd3.css";
+import "/public/assets/css/animated-headlines7dd3.css";
+import "/public/assets/css/style7dd3.css";
+
+// JS (static, loaded globally)
+const scriptUrls = [
+  "/public/assets/js/jquery7dd3.js",
+  "/public/assets/js/isotope.js",
+  "/public/assets/js/magnific.js",
+  "/public/assets/js/animated-headlines7dd3.js",
+  "/public/assets/js/waypoints7dd3.js",
+  "/public/assets/js/app.js",
+  "/public/assets/js/init7dd3.js",
+];
+
+scriptUrls.forEach((url) => {
+  const script = document.createElement("script");
+  script.src = url;
+  script.async = false;
+  document.body.appendChild(script);
+});
+
+// React render
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
