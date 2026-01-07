6c5c57eff1413cc48fc95f146567577ba63df033
madanbio <madanuideveloper@gmail.com>
2026-01-07 10:54:29 +0530

    Fix Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/madanbio.github.io/",
-  build: {
-    outDir: "root",   // 👈 GitHub Pages friendly
-  },
+  base: "/",        // ✅ correct for username.github.io repo
 });
