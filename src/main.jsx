5e761e1cf2acfedb1250c23b0c011a1ffbb0653a
madanbio <madanuideveloper@gmail.com>
2026-01-07 12:48:17 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '/public/assets/css/base7dd3.css'
+import '/public/assets/css/magnific7dd3.css'
+import '/public/assets/css/animated-headlines7dd3.css'
+import '/public/assets/css/style7dd3.css'
+import App from './App.jsx'
 
-// CSS
-import "/public/assets/css/base7dd3.css";
-import "/public/assets/css/magnific7dd3.css";
-import "/public/assets/css/animated-headlines7dd3.css";
-import "/public/assets/css/style7dd3.css";
-
-// JS (static, loaded globally)
-const scriptUrls = [
-  "/public/assets/js/jquery7dd3.js",
-  "/public/assets/js/isotope.js",
-  "/public/assets/js/magnific.js",
-  "/public/assets/js/animated-headlines7dd3.js",
-  "/public/assets/js/waypoints7dd3.js",
-  "/public/assets/js/app.js",
-  "/public/assets/js/init7dd3.js",
-];
-
-scriptUrls.forEach((url) => {
-  const script = document.createElement("script");
-  script.src = url;
-  script.async = false;
-  document.body.appendChild(script);
-});
-
-// React render
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
