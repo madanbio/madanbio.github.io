144cd3d26c3a54e38534a50d2369c1d8eb764dda
madanbio <madanuideveloper@gmail.com>
2026-01-07 10:49:15 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/madanbio.github.io",   // 👈 IMPORTANT
+  base: "/madanbio.github.io/",
+  build: {
+    outDir: "root",   // 👈 GitHub Pages friendly
+  },
 });
